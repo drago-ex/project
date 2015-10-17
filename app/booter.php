dd36e08107976ab102bcdd6a600b36dfc310e641
Zdeněk Papučík <zdenek.papucik@gmail.com>
2015-10-17 13:01:21 +0200

    dev

---
+++ app/booter.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Drago Framework
+ * Copyright (c) 2015, Zdeněk Papučík
+ */
+require __DIR__ . '/libs/Drago/loader.php';
+
+// Configure application.
+$app = new Drago\Configurator();
+
+// Enable debagger bar.
+$app->enableDebugger(__DIR__ . '/log');
+
+// Temporary directory.
+$app->setTempDirectory(__DIR__ . '/storage');
+
+// Enabled autoload classes.
+$app->addAutoload(__DIR__);
+
+// Create DI container from configuration files.
+$app->addFindConfig(__DIR__ . '/modules');
+$app->addConfig(__DIR__ . '/app.neon');
+
+// Run application.
+$app->run();
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This file is part of the Drago Framework
+ * Copyright (c) 2015, Zdeněk Papučík
+ */
+require __DIR__ . '/app/booter.php';
