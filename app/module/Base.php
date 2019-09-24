f728a5d0cc4b546c88347ba6c782a5006d65b6f7
accgit <zdenek.papucik@gmail.com>
2019-09-24 06:46:40 +0200

    Update

--- app/module/Base.php
+++ app/module/Base.php
@@ -4,26 +4,10 @@ declare(strict_types = 1);
 
 namespace App;
 
-use Drago\Parameters\Environment;
 use Nette\Application\UI\Presenter;
 
 
 /**
  * Base class for all modules.
  */
-abstract class BasePresenter extends Presenter
-{
-	/**
-	 * @var Environment
-	 * @inject
-	 */
-	public $environment;
-
-
-	protected function startup(): void
-	{
-		parent::startup();
-		$mode = $this->environment->isProduction();
-		$mode ? $this->setLayout('layout') : $this->setLayout('dev');
-	}
-}
+abstract class BasePresenter extends Presenter {}
