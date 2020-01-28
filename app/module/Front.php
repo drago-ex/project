040400f4d70b3eb614cd9610c26597614eb7b35d
Zdeněk Papučík <zdenek.papucik@gmail.com>
2020-01-28 13:09:27 +0100

    Update Front.php

--- app/module/Front.php
+++ app/module/Front.php
@@ -5,14 +5,13 @@ declare(strict_types = 1);
 namespace App;
 
 use Drago\Parameters\Parameters;
-use Nette\Application\UI\Presenter;
 
 
 trait Front
 {
-	public function injectOnStartup(Parameters $parameters, Presenter $presenter): void
+	public function injectOnStartup(Parameters $parameters): void
 	{
-		$presenter->onStartup[] = function () use ($parameters) {
+		$this->onStartup[] = function () use ($parameters) {
 			$locale = $parameters->getAppDir();
 			$this->translateFile = $locale . '/locale/';
 		};
