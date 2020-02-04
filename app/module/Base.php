244f6fb6efbc0069901d2c381100663aafb6223d
Zdeněk Papučík <zdenek.papucik@gmail.com>
2020-02-04 08:35:07 +0100

    Update Base.php

--- app/module/Base.php
+++ app/module/Base.php
@@ -4,37 +4,10 @@ declare(strict_types = 1);
 
 namespace App;
 
-use Drago\Localization\Locale;
-use Drago\Localization\Translator;
-use Nette\Bridges\ApplicationLatte\Template;
-use stdClass;
+use Drago\Localization\TranslatorAdapter;
 
-/**
- * @property-read Template|stdClass $template
- */
+
 trait Base
 {
-	use Locale;
-
-	/** @var string */
-	public $translateFile;
-
-
-	public function injectOnRender(): void
-	{
-		$this->onRender[] = function () {
-			$this->template->lang = $this->lang;
-			$this->template->setTranslator($this->getTranslator());
-		};
-	}
-
-
-	/**
-	 * @throws \Exception
-	 */
-	public function getTranslator(): Translator
-	{
-		$file = $this->translateFile . $this->lang;
-		return $this->createTranslator($file);
-	}
+	use TranslatorAdapter;
 }
