bf511dff4b2733aea49297d3a97dcac05eabb0a1
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-02-25 10:39:50 +0100

    Update and rename index.php to www/index.php

--- www/index.php
+++ www/index.php
@@ -3,7 +3,7 @@
 declare(strict_types = 1);
 
 // Composer autoload.
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Run application.
 $boot = App\Bootstrap::boot();
