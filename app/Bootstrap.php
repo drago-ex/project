a0130175690310ea8799c85f02f1e4bdd798ee1a
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-02-25 10:37:41 +0100

    Update Bootstrap.php

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -15,18 +15,19 @@ class Bootstrap
 	public static function boot(): ExtraConfigurator
 	{
 		$app = new ExtraConfigurator;
+		$appDir = dirname(__DIR__);
 
 		// Enable debug mode.
 		//$app->setDebugMode('127.0.0.1');
 
 		// Enable Tracy tool.
-		$app->enableTracy(__DIR__ . '/../log');
+		$app->enableTracy($appDir . '/log');
 
 		// Set the time zone.
 		$app->setTimeZone('Europe/Prague');
 
 		// Directory of temporary files.
-		$app->setTempDirectory(__DIR__ . '/../storage');
+		$app->setTempDirectory($appDir . '/storage');
 
 		// Auto-loading classes.
 		$app->createRobotLoader()
