f08d6c13f9e9f337d0251988e7b4d0ee8260b60a
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-03-18 12:49:04 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ mode }) => {
+	const DEV = mode === 'development';
+
+	return {
+		publicDir: './assets/public',
+		base: '/dist/',
+		server: {
+			open: false,
+			hmr: false,
+		},
+		css: {
+			postcss: [
+				"autoprefixer"
+			]
+		},
+		build: {
+			assetsDir: '',
+			outDir: './www/dist/',
+			emptyOutDir: true,
+			minify: DEV ? false : 'esbuild',
+			rollupOptions: {
+				output: {
+					manualChunks: undefined,
+					chunkFileNames: '[name].js',
+					entryFileNames: '[name].js',
+					assetFileNames: '[name].[ext]',
+				},
+				input: {
+					app: './assets/app.js',
+				}
+			}
+		},
+	}
+});
