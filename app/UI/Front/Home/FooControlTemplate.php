9cb8d9a7a3009c63a600a5a98866e7648b4443d1
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-03-26 10:23:22 +0100

    Create FooControlTemplate.php

---
+++ app/UI/Front/Home/FooControlTemplate.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Front\Home;
+
+use Drago\Application\UI\ExtraTemplate;
+
+
+class FooControlTemplate extends ExtraTemplate
+{
+	public string $offcanvasId;
+	public string $modalId;
+	public string $random;
+}
