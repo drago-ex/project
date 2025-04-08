e5c036131a33fd47e295a2ae96e2ff40af8d0c0b
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-04-08 08:54:58 +0200

    Update SignRecoveryToken.php

--- app/UI/Backend/Sign/SignRecoveryToken.php
+++ app/UI/Backend/Sign/SignRecoveryToken.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace App\UI\Backend\Sign;
 
 
+/**
+ * This class represents the recovery token and its associated validation flag.
+ * It is used to store the state of the recovery token and whether the token has been checked.
+ */
 class SignRecoveryToken
 {
 	public function __construct(
-		public ?string $token = null,
+		public ?bool $token = null,
 		public ?bool $tokenCheck = null,
 	) {
 	}
