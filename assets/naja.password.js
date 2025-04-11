380590bb9df5542e72b0076ce246bd9060cb4473
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-04-11 19:11:45 +0200

    Update naja.password.js

--- assets/naja.password.js
+++ assets/naja.password.js
@@ -1,51 +1,24 @@
 export default class PasswordToggle {
 	initialize(naja) {
-		const getPasswordElements = (element) => {
-			const input = element.querySelector('.input-password');
-			const button = element.querySelector('.toggle-password');
-			return { input, button };
-		};
+		const attachPasswordToggle = (root) => {
+			root.querySelectorAll('.input-group').forEach((group) => {
+				const input = group.querySelector('.input-password');
+				const button = group.querySelector('.toggle-password');
 
-		const togglePassword = (input) => {
-			input.type = input.type === 'password' ? 'text' : 'password';
-		};
-
-		const toggleButtonVisibility = (button, isVisible) => {
-			if (isVisible) {
-				button.classList.remove('d-none');
-			} else {
-				button.classList.add('d-none');
-			}
-		};
-
-		const addPasswordToggle = (doc) => {
-			doc.querySelectorAll('.input-group').forEach((element) => {
-				const { input, button } = getPasswordElements(element);
-				if (input) {
-					const isPasswordVisible = input.type === 'password';
-					toggleButtonVisibility(button, isPasswordVisible);  // Toggle button visibility based on input type
-
-					if (button && !button._isListenerAdded) {
-						button.addEventListener('click', () => togglePassword(input));
-						button._isListenerAdded = true;
-					}
+				if (input && button && !button._isListenerAdded) {
+					button.addEventListener('click', () => {
+						const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+						input.setAttribute('type', type);
+					});
+					button._isListenerAdded = true;
+					button.classList.remove('d-none');
 				}
 			});
 		};
 
-		addPasswordToggle(document);
+		attachPasswordToggle(document);
 		naja.snippetHandler.addEventListener('afterUpdate', (e) => {
-			addPasswordToggle(e.detail.snippet);
-		});
-
-		naja.addEventListener('complete', () => {
-			document.querySelectorAll('.input-group').forEach((element) => {
-				const { input, button } = getPasswordElements(element);
-				if (input && button) {
-					const isPasswordVisible = input.type === 'password';
-					toggleButtonVisibility(button, isPasswordVisible);
-				}
-			});
+			attachPasswordToggle(e.detail.snippet);
 		});
 	}
 }
