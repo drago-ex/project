f1d7d2d496e31c5ae33e156acb92b90504d2a577
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-04-23 06:21:10 +0200

    Update Forms.php

--- app/Core/Form/Forms.php
+++ app/Core/Form/Forms.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Core\Form;
 
-use InvalidArgumentException;
 use Nette\Application\UI\Form;
 
 
@@ -121,47 +120,6 @@ class Forms extends Form
 			type: 'password',
 			placeholder: 'Your password',
 			required: 'Please enter your password.',
-		);
-	}
-
-
-	/**
-	 * Adds a password confirmation input field to the form.
-	 */
-	public function addPasswordConfirmationField(): Inputs
-	{
-		// Create a password confirmation input field
-		$passwordField = $this->addTextInput(
-			name: 'verify',
-			label: 'Password to check',
-			type: 'password',
-			placeholder: 'Re-enter password',
-			required: 'Please enter your password to check.',
-		);
-
-		// Check if 'password' field exists in the form
-		if (!isset($this['password'])) {
-			throw new InvalidArgumentException('Password field is required for password confirmation.');
-		}
-
-		// Add the rule to check if the 'verify' field matches the 'password' field
-		$passwordField->addRule($this::Equal, 'Passwords do not match.', $this['password']);
-
-		return $passwordField;
-	}
-
-
-	/**
-	 * Adds an email input field to the form.
-	 */
-	public function addEmailField(): Inputs
-	{
-		return $this->addTextInput(
-			name: 'email',
-			label: 'Email',
-			type: 'email',
-			placeholder: 'Email address',
-			required: 'Please enter your email address.',
 		);
 	}
 }
