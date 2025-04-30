4ff24ddde00779f5bef990cb5e933c70df3b1d89
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-04-30 06:06:24 +0200

    Create SignSender.php

---
+++ app/UI/Backend/Sign/SignSender.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\UI\Backend\Sign;
+
+use App\Core\User\User;
+use Latte\Engine;
+use Nette\Mail\Mailer;
+use Nette\Mail\Message;
+use Tracy\Debugger;
+
+
+readonly class SignSender
+{
+	public function __construct(
+		private Mailer $mailer,
+		private User $user,
+	) {
+	}
+
+
+	public function createEmail(string $email, string $subject): void
+	{
+		$message = new Message();
+		$message->setFrom('no-reply@example.com')
+			->addTo($email)
+			->setSubject($subject)
+			->setHtmlBody((new Engine)->renderToString(__DIR__ . '/email.latte', [
+				'user' => $this->user,
+			]));
+
+		try {
+			$this->mailer->send($message);
+
+		} catch (\Throwable $e) {
+			Debugger::log($e, Debugger::ERROR);
+		}
+	}
+}
