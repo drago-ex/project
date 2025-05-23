5e37704281614c627ab48319aee03d8ad4908f3e
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-05-23 12:28:47 +0200

    Update SignSender.php

--- app/UI/Backend/Sign/SignSender.php
+++ app/UI/Backend/Sign/SignSender.php
@@ -4,31 +4,39 @@ declare(strict_types=1);
 
 namespace App\UI\Backend\Sign;
 
-use App\Core\User\User;
 use Latte\Engine;
 use Nette\Mail\Mailer;
 use Nette\Mail\Message;
 use Tracy\Debugger;
 
 
-readonly class SignSender
+class SignSender
 {
+	// User email.
+	public string $email;
+
+	// Email subject.
+	public string $subject;
+
+	// Password recovery token.
+	public string $token;
+
+
 	public function __construct(
-		private Mailer $mailer,
-		private User $user,
+		private readonly Mailer $mailer,
 	) {
 	}
 
 
-	public function createEmail(string $email, string $subject): void
+	public function sendEmail(): void
 	{
 		$message = new Message();
-		$message->setFrom('no-reply@example.com')
-			->addTo($email)
-			->setSubject($subject)
+		$message->setFrom('no-reply@scrs.site')
+			->addTo($this->email)
+			->setSubject($this->subject)
 			->setHtmlBody((new Engine)->renderToString(__DIR__ . '/email.latte', [
-				'user' => $this->user,
-				'subject' => $subject,
+				'subject' => $this->subject,
+				'token' => $this->token,
 			]));
 
 		try {
