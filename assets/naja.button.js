556494c53ffcb5f9159acc2b9248a76291a55fe1
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-05-30 06:06:50 +0200

    Update naja.button.js

--- assets/naja.button.js
+++ assets/naja.button.js
@@ -1,35 +1,27 @@
 export default class SubmitButtonDisable {
 	initialize(naja) {
-		let submitButton = null;
-
 		const submitDisable = (doc) => {
 			doc.querySelectorAll('form').forEach(form => {
-				const submits = form.querySelectorAll('[data-btn-submit]');
-				submits.forEach(submit => {
-					submit.addEventListener('click', () => {
-						if (form && form.checkValidity()) {
-							submitButton = submit;
-						}
-					});
-				});
+				if (form._submitListener) {
+					form.removeEventListener('submit', form._submitListener);
+				}
+
+				const listener = (event) => {
+					if (form.checkValidity()) {
+						form.querySelectorAll('[data-btn-submit]').forEach(btn => btn.disabled = true);
+					} else {
+						event.preventDefault();
+					}
+				};
+
+				form.addEventListener('submit', listener);
+				form._submitListener = listener;
 			});
 		};
 
 		submitDisable(document);
 		naja.snippetHandler.addEventListener('afterUpdate', (e) => {
 			submitDisable(e.detail.snippet);
-		});
-
-		naja.addEventListener('start', () => {
-			if (submitButton) {
-				submitButton.disabled = true;
-			}
-		});
-
-		naja.addEventListener('complete', () => {
-			if (submitButton) {
-				submitButton.disabled = false;
-			}
 		});
 	}
 }
